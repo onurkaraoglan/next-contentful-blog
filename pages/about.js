6ac6a5f5c1b450765a7e3655ed14032c9fce5fc6
onurkaraoglan <okaraoglan91@gmail.com>
2021-11-25 18:19:52 +0300

    about style improvement

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,17 @@ import styles from "../styles/Home.module.css";
 export default function About() {
   const d = new Date();
   let year = d.getFullYear();
+  let tags = [
+    "React",
+    "JavaScript",
+    "TypeScript",
+    "GoLang",
+    "AngularJS",
+    "ASP.Net MVC",
+    "AWS",
+    "DigitalOcean",
+    "CI/CD",
+  ];
   return (
     <Layout>
       <div className={styles.container}>
@@ -18,13 +29,32 @@ export default function About() {
           experience and a Bachelor’s degree in Computer Science. I have
           hands-on experience using various technologies like JavaScript(ES6),
           TypeScript, React.js and Golang to create and implement software
-          applications. Also, I am familiar with AngularJS and .Net MVC. I use
-          version control systems and CI/CD pipelines with my projects which run
-          on cloud platforms like AWS, DigitalOcean and VSTS(Azure DevOps). I
-          enjoy being challenged and engaging with projects that require me to
+          applications.
+        </p>
+        <p className={styles.content}>
+          {" "}
+          Also, I am familiar with AngularJS and .Net MVC. I use version control
+          systems and CI/CD pipelines with my projects which run on cloud
+          platforms like AWS, DigitalOcean and VSTS(Azure DevOps).
+        </p>
+        <p className={styles.content}>
+          I enjoy being challenged and engaging with projects that require me to
           work outside of my comfort and knowledge set. Continuing to learn new
           languages and development techniques is important to me.
         </p>
+
+        <div>
+          <p style={{ display: "inline" }}>Keywords:</p>
+          <div className={styles.about_tags}>
+            {tags.map((tag) => {
+              return (
+                <div key={tag} className={styles.tag}>
+                  {tag}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </Layout>
   );
